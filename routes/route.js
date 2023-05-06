1965ef004b3f8c259e0ab3003475b3a5ee52d0e7
Ankit Kumar Verma <110284078+Avverma9@users.noreply.github.com>
2023-05-06 23:12:19 +0530

    backend done

--- routes/route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router();
-const userController = require("../controllers/userController")
-const loginController = require("../controllers/loginController")
-
-router.post("/submit",userController.storeUserDetails) //contact
-router.get("/get",loginController.login)//admin
-
-module.exports = router
